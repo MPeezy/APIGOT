d12cd9993dec81233b809a3aa69e2001f1b838f4
MPeezy <marjoriepatton@rocketmortgage.com>
2022-01-13 17:23:29 -0500

    initial

---
+++ Assessment9/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Assessment9.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assessment9.Controllers
+{
+    public class HomeController : Controller
+    {
+        ThronesDAL dal = new ThronesDAL();
+
+        public IActionResult Index()
+        {
+            List<Character> characters = new List<Character>();
+            characters.Add(dal.GetSpecificCharacter(583));
+            characters.Add(dal.GetSpecificCharacter(238));
+            characters.Add(dal.GetSpecificCharacter(339));
+            characters.Add(dal.GetSpecificCharacter(2));
+            characters.Add(dal.GetSpecificCharacter(100));
+
+            return View(characters);
+        }
+
+      
+  
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Assessment9/Models/ThronesDAL.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Assessment9.Models
+{
+    public class ThronesDAL
+    {
+        public Character GetSpecificCharacter(int id)
+        {
+            string url = $"https://anapioficeandfire.com/api/characters/" + id;
+
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            StreamReader rd = new StreamReader(response.GetResponseStream());
+
+            string output = rd.ReadToEnd();
+
+            rd.Close();
+
+            Character c = JsonConvert.DeserializeObject<Character>(output); 
+
+            return c;
+
+
+        }
+    }
+}
